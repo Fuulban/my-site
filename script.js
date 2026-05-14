567291033fa67282bd9c3340d7263acb302840d4
Fuulban <zapasak71@gmail.com>
2026-05-14 09:26:23 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// variables
+let screenWidth = window.innerWidth;
+let screenHeight = window.innerHeight;
 
+// width > height?
+if (screenWidth > screenHeight) {
+  document.querySelector('content').innerHTML = '<table cullspace="50"><tr><td><h1>Привет!</h1><p>Это страничка для обучения.</p></td><td><h1>С помощью этой странички мы</h1><p> • Изучаем базовую разметку на HTML<br> • Стилизовываем сайт через CSS<br>Добавляем механику через JS</p></td></tr></table>'
+)
