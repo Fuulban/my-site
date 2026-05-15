4f59b99384196b10e3e0cc453bd091c636f5602f
Fuulban <zapasak71@gmail.com>
2026-05-15 10:25:28 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-// variables
-let screenWidth = window.innerWidth;
-let screenHeight = window.innerHeight;
-const content = document.getElementById('content');
 
-// width > height?
-if (screenWidth > screenHeight) {
-  content.innerHTML = '<table cellspacing="50"><tr><td><h1>Привет!</h1><p>Это страничка для обучения.</p></td><td><h1>С помощью этой странички мы</h1><p> • Изучаем базовую разметку на HTML<br> • Стилизовываем сайт через CSS<br> • Добавляем механику через JS</p></td></tr></table>';
-};
