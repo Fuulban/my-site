3076ea002938c3c85648d4aa61b2f61531b3c8e9
Fuulban <zapasak71@gmail.com>
2026-05-15 10:06:57 +0400

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ let screenHeight = window.innerHeight;
 
 // width > height?
 if (screenWidth > screenHeight) {
-  document.querySelector("content").innerHTML = '<table cellspacing="50"><tr><td><h1>Привет!</h1><p>Это страничка для обучения.</p></td><td><h1>С помощью этой странички мы</h1><p> • Изучаем базовую разметку на HTML<br> • Стилизовываем сайт через CSS<br>Добавляем механику через JS</p></td></tr></table>'
+  document.querySelector("content").innerHTML = '<table cellspacing="50"><tr><td><h1>Привет!</h1><p>Это страничка для обучения.</p></td><td><h1>С помощью этой странички мы</h1><p> • Изучаем базовую разметку на HTML<br> • Стилизовываем сайт через CSS<br> • Добавляем механику через JS</p></td></tr></table>'
 }
