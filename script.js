dfecc14ea2e4811a29a50c6cd135bd7f7c22906e
Fuulban <zapasak71@gmail.com>
2026-05-17 17:23:23 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// onstart
+alert('Сайт тестовый, и предназначен исключительно для примера');
 
+// const/variable
+const htmlContent = document.GetElementById('content');
+let screenHeight = window.innerHeight;
+let screenWidth = window.innerWidth;
+
+// functions
+// no function
+
+// main code
+if (screenWidth > screenHeight) {
+  htmlContent.innerHTML = '
