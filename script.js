e18687a9b8f5fc3e4a608c55f725fdcb2a3e1daa
Fuulban <zapasak71@gmail.com>
2026-05-17 17:54:06 +0400

    Update script.js

--- script.js
+++ script.js
@@ -11,12 +11,5 @@ let screenWidth = window.innerWidth;
 
 // main code
 if (screenWidth > screenHeight) {
-  htmlContent.innerHTML = '<table cellspacing="50">' +
-    '<tr>' +
-      '<td>' +
-        '<h1>Привет!</h1>' +
-        '<p>Эта страничка для обучения и теста.</p>' +
-      '</td>' +
-      '<td>' +
-        '<h1>С помощью этой страницы мы</h1><p>• Обучаем новичков базовой разметке<br>• Стилизуем сайт через CSS<br>• Добавляем контент страницы через JS</p></td></tr></table>';
+  htmlContent.innerHTML = '<table cellspacing="50"><tr><td><h1>Привет!</h1><p>Эта страничка для обучения и теста.</p></td><td><h1>С помощью этой страницы мы</h1><p>• Обучаем новичков базовой разметке<br>• Стилизуем сайт через CSS<br>• Добавляем контент страницы через JS</p></td></tr></table>';
 }
